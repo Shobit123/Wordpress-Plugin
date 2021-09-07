ff89ef09e9571fd5abf1b6c7e82fcc904743ec2b
shobit singh <38094214+Shobit123@users.noreply.github.com>
2021-09-07 16:25:19 +0530

    Add files via upload

---
+++ Multivendor/includes/footer.php
@@ -0,0 +1,60 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <h4> Pages </h4>
+                <ul>
+                    <li><a href="cart.php">Shopping Cart</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="checkout.php">My Account</a></li>
+                </ul>
+                <hr>
+                <h4>User Selection</h4>
+                <ul>
+                    <li><a href="checkout.php">Login</a></li>
+                    <li><a href="customer_registration.php">Register</a></li>
+                </ul>
+                <hr class="hidden-md hidden-lg hidden-sm">
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h4>Top Product</h4>
+                <ul>
+                    <li><a href="#">Jacket</a></li>
+                    <li><a href="#">Accessories</a></li>
+                    <li><a href="#">Shoes</a></li>
+                    <li><a href="#">Coats</a></li>
+                    <li><a href="#">T-Shirt</a></li>
+                </ul>
+                <hr class="hidden-md hidden-lg">
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h4>Where to find Us</h4>
+                <p>
+                    <strong>Test.com</strong>
+                    <b>Shobit</b>
+                    <b>Mirzapur</b>
+                    <b>Uttar pradesh</b>
+                    <b>+91 9999999999</b>
+                </p>
+                <a href="contact.php">GoTo ContactUs Page</a>
+                <hr class="hidden-md hidden-lg">
+            </div>
+            <div class="col-md-3 col-sm-6">
+                <h4>Get the News</h4>
+                <p class="text-muted">
+                    subscribe here getting news of Uttar Pradesh
+                </p>
+                <form action="" method="post">
+                    <div class="input-group">
+                        <input type="text" name="email" class="form-control">
+                        <span class="input-group-btn">
+                            <input type="submit" class="btn btn-default" value="subscribe">
+                        </span>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
